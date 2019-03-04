d40ce53927f49af8ffa5a48d301dbf108f117d58
mxwilson <matthewxwilson@gmail.com>
2019-03-03 21:02:33 -0500

    browser and xdotool

--- 1.sh
+++ 1.sh
@@ -122,10 +122,87 @@ else
 	done
 fi
 
-#which browser
-#.config/autostart/desktopfile
-#custom.conf
-#xdo?
+#launch browser on boot
+while [ -z "$browserq" ] ; do
+	read -p "Launch Firefox automatically upon booting? (y/n): " browserq
+
+	if [ ! -z "$browserq" ] ; then
+		if [[ "$browserq" == "y" || "$browserq" == "yes" ]] ; then
+			echo "OK let's use firefox."
+			firefoxuse=1;
+		else	
+			echo "OK nevermind!"
+			echo "Done"
+			exit 0
+		fi
+	fi
+done
+
+
+outputfile_autostart_script=$( cat << EOF 
+#!/bin/bash
+firefox --url "https://radar.weather.gov/Conus/full_loop.php" &
+sleep 3;\n
+EOF
+)
+
+outputfile_autostart_dotdesktop=$( cat << EOF 
+[Desktop Entry]
+Type=Application
+Name=kiosk-autostart
+Exec=/home/$response/browser-autostart.sh\n
+EOF
+)
+
+MYSCRIPT="/home/$response/browser-autostart.sh"
+MYDESKTOPFILE="/home/$response/.config/autostart/kiosk-autostart.desktop"
+
+if [ "$firefoxuse" == "1" ] ; then
+	printf "$outputfile_autostart_script" > $MYSCRIPT
+
+	if [ $? != 0 ] ; then 
+		echo "Error: unable to write $MYSCRIPT. Exiting."	
+		exit 1
+	fi
+
+	echo "Placing script at: $MYSCRIPT"	
+	chmod +x  $MYSCRIPT
+	chown $response:$response $MYSCRIPT
+
+	if [ ! -e "/home/$response/.config/autostart" ] ; then 
+		mkdir -pv /home/$response/.config/autostart
+	fi
+
+	printf "$outputfile_autostart_dotdesktop" > $MYDESKTOPFILE
+
+	if [ $? != 0 ] ; then 
+		echo "Error: unable to write $MYDESKTOPFILE. Exiting."	
+		exit 1
+	fi
+
+	echo "Placing desktop file at: $MYDESKTOPFILE"	
+	chown $response:$response -R /home/$response/.config/autostart/
+
+	#install xdo tool
+	while [ -z "$xdoq" ] ; do
+		read -p "Install \"xdotool\" to simulate \"F11\" key press (for fullscreen browser)? (y/n):" xdoq
+
+		if [ ! -z "$xdoq" ] ; then
+			if [[ "$xdoq" == "y" || "$xdoq" == "yes" ]] ; then
+				if [ "$DISTRO" == "ubuntu" ] ; then
+					apt install xdotool
+				else
+					yum -y install epel-release && rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+					yum -y install xdotool
+				fi
+			echo "xdotool key \"F11\"" >> $MYSCRIPT
+			else	
+				echo "OK nevermind!"
+				break;
+			fi
+		fi
+	done
+fi
 
 echo "Done"
 exit 0
